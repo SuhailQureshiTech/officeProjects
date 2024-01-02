5bb92f8a67ecbfac1a5603fe6b1e9238b5dfeb15
SuhailQureshi <suhailqureshi.ibl@gmail>
2024-01-02 11:55:51 +0500

    02Jan24

--- dagsDevelopment/test.py
+++ dagsDevelopment/test.py
@@ -1,9 +1,16 @@
 
+# import datetime
+from datetime import date, datetime,timedelta,timezone
 import sys
 import os
 from suhailLib import ftpUploadDirTransfer,returnDataDate
 
-vFirstDate,vLastDate=returnDataDate(15)
+# vFirstDate,vLastDate=returnDataDate(15)
+# print(vFirstDate.strftime("%d-%b-%Y"))
+# print(vLastDate.strftime("%d-%b-%Y"))
 
-print(vFirstDate.strftime("%d-%b-%Y"))
-print(vLastDate.strftime("%d-%b-%Y"))
+utc = timezone.utc
+date = datetime.now(utc)
+print('utc Time : ', date)
+creationDate = date + timedelta(hours=5)
+print(creationDate)
